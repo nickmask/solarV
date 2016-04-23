71fe689b0a8d1d3641a70d8ce380d1517eb12418
Nick Maskell <nrmaskell@gmail.com>
2016-04-24 11:51:32 +1200

    Update navbar

--- src/modules/Navbar.js
+++ src/modules/Navbar.js
@@ -1,50 +1,37 @@
-import React from 'react'
 
-var NavBrand = React.createClass({
-  render: function(){
+import React, { Component, PropTypes } from 'react'
+import { Link, IndexLink } from 'react-router'
+import { Row, Col } from 'react-bootstrap'
+import NavLink from './NavLinks'
+import classNames from 'classnames'
+require('../www/stylesheets/nav.sass')
+
+class NavMenu extends Component {
+
+  render () {
+    const mobileClasses = classNames('mobileDiv', this.state.expanded)
     return (
-      <a className="navbar-brand" href={this.props.linkTo}>{this.props.text}</a>
-    );
+      <div className='nav navbar-fixed-top'>
+        <div className='container'>
+          <Row >
+            <Col sm={5} >
+              <h1>Solar V</h1>
+            </Col>
+            <Col sm={7}>
+              <div >
+                <ul>
+                  <li><IndexLink activeClassName='active' to='/'>{'HOME'}</IndexLink></li>
+                  <li><NavLink to='/why-solor'>{'PROJECTS'}</NavLink></li>
+                  <li><NavLink to='/about-us'>{'BLOG'}</NavLink></li>
+                  <li><NavLink to='/get-in-touch'>{'ABOUT'}</NavLink></li>
+                </ul>
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </div>
+    )
   }
-});
+}
 
-var NavMenu = React.createClass({
-  render: function(){
-    var links = this.props.links.map(function(link){
-      if(link.dropdown) {
-        return (
-          <NavLinkDropdown links={link.links} text={link.text} active={link.active} />
-        );
-      }
-      else {
-        return (
-          <NavLink linkTo={link.linkTo} text={link.text} active={link.active} />
-        );
-      }
-    });
-    return (
-      <ul className="nav navbar-nav">
-        {links}
-      </ul>
-    );
-  }
-});
-
-var NavLink = React.createClass({
-  render: function(){
-    return(
-      <li className={(this.props.active ? "active" : "")}><a href={this.props.linkTo}>{this.props.text}</a></li>
-    );
-  }
-});
-
-// set data
-var navbar = {};
-navbar.brand =
-  {linkTo: "/", text: "Solar V"};
-navbar.links = [
-  {linkTo: "/", text: "Home"},
-  {linkTo: "/why-solar", text: "Why solar?"},
-  {linkTo: "/about-us", text: "Link 2"},
-  {linkTo: "/get-in-touch", text: "Get in touch"},
-]
+export default NavMenu

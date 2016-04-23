e46a45eeba7a8bb914b780dccea0980f3e843a12
Nick Maskell <nrmaskell@gmail.com>
2016-04-24 11:56:50 +1200

    Tidy why file

--- src/modules/Why-solar.js
+++ src/modules/Why-solar.js
@@ -5,3 +5,23 @@ export default React.createClass({
     return <div>Why solar?</div>
   }
 })
+
+import React, { PropTypes, Component } from 'react'
+import { Row, Col } from 'react-bootstrap'
+require('../www/stylesheets/blog.sass')
+
+class Why extends Component {
+  render () {
+    return (
+      <Row >
+        <Col sm={12}>
+          <div>
+            <h1>Coming soon...</h1>
+          </div>
+        </Col>
+      </Row>
+    )
+  }
+}
+
+export default Why

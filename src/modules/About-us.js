c2837ca28593ca6c6853cc84d5463052910d65d6
Nick Maskell <nrmaskell@gmail.com>
2016-04-30 17:26:53 +1200

    Add simple about us

--- src/modules/About-us.js
+++ src/modules/About-us.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { PropTypes, Component } from 'react'
+import { Row, Col } from 'react-bootstrap'
 
-export default React.createClass({
-  render() {
-    return <div>About us</div>
+class Why extends Component {
+  render () {
+    return (
+      <Row >
+        <Col sm={12}>
+          <div>
+            <h1>About Us</h1>
+          </div>
+        </Col>
+      </Row>
+    )
   }
-})
+}
+
+export default Why

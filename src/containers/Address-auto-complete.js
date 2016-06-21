8ad18abb8ee2c11b6f0228f0ed6a98466d5e9194
Nick Maskell <nrmaskell@gmail.com>
2016-06-21 17:16:55 +1200

    Auto correct functional

--- src/containers/Address-auto-complete.js
+++ src/containers/Address-auto-complete.js
@@ -1,19 +1,33 @@
 import React, { PropTypes, Component } from 'react'
 import { Link } from 'react-router'
 import { Col } from 'react-bootstrap'
-import Geosuggest from 'react-geosuggest'
+import { GoogleMap, GoogleMapLoader, Marker, SearchBox } from "react-google-maps";
 
 export default class Testing extends Component {
+    componentDidMount () {
+    var input = document.getElementById('searchTextField');
+    var options = {componentRestrictions: {country: 'NZ'}};
+    new google.maps.places.Autocomplete(input, options);
+  }
+
+  buttonClick () {
+    alert(this.refs.searchField.getDOMNode().value);
+  }
+
   render () {
     return (
       <Col sm={4} panel>
         <h2>Address</h2>
-        <Geosuggest
-        placeholder='Start typing!'
-        initialValue='Hamburg'
-        location={new google.maps.LatLng(53.558572, 9.9278215)}
-        radis='20' />
-      </Col>
+        <div>
+          <label htmlFor="searchTextField">
+            Please Insert an address:
+          </label>
+          <br/>
+          <input ref='searchField' id="searchTextField" type="text" size="50"/>
+          <br/>
+          <button onClick={this.buttonClick}>Submit</button>
+          </div>
+        </Col>
     )
   }
 }
